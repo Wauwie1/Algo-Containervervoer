712292358b4cf43cb396285a9991ddd9e0e4bf25
Paul <paulusman@live.nl>
2018-09-19 15:35:38 +0200

    Added Sorter class and SortByWeight method
    Unit tested

---
+++ Algo-containervervoer/Algo-containervervoer/Sorter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_containervervoer
+{
+    public class Sorter
+    {
+        public List<Container> unplacedContainers { get; private set; }
+
+        public Sorter(List<Container> containersToBePlaced)
+        {
+            unplacedContainers = containersToBePlaced;
+            SortByWeight();
+        }
+
+        private void SortByWeight()
+        {
+
+              unplacedContainers = unplacedContainers.OrderBy(c => c.weight).ToList();
+            
+        }
+
+    }
+}
---
+++ Algo-containervervoer/Algo-containervervoerTests/SorterTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo_containervervoer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_containervervoer.Tests
+{
+    [TestClass()]
+    public class SorterTests
+    {
+        [TestMethod()]
+        public void SorteerTest()
+        {
+            List<Container> unplacedContainers = new List<Container>();
+            
+            Container container1 = new Container(99, Container.ContainerType.Regular);
+            Container container2 = new Container(80, Container.ContainerType.Cooled);
+            Container container3 = new Container(11, Container.ContainerType.Precious);
+            Container container4 = new Container(54, Container.ContainerType.Regular);
+            Container container5 = new Container(90, Container.ContainerType.Cooled);
+            Container container6 = new Container(120, Container.ContainerType.Regular);
+
+            unplacedContainers.Add(container1);
+            unplacedContainers.Add(container2);
+            unplacedContainers.Add(container3);
+            unplacedContainers.Add(container4);
+            unplacedContainers.Add(container5);
+            unplacedContainers.Add(container6);
+
+            unplacedContainers = unplacedContainers.OrderByDescending(c => c.weight).ToList();
+            Console.WriteLine(unplacedContainers[0].weight);
+            Assert.AreEqual(120, unplacedContainers[0].weight);
+
+        }
+    }
+}
