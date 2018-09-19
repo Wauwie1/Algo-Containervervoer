e0d22d3f187960e7a70a98f5988e59d111d862b8
Paul <paulusman@live.nl>
2018-09-19 13:32:10 +0200

    Added container class

---
+++ Algo-containervervoer/Algo-containervervoer/Container.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_containervervoer
+{
+    class Container
+    {
+        public int weight { get; private set; }
+        public ContainerType type { get; private set; }
+
+        public Container(int weight, ContainerType type)
+        {
+            this.weight = weight;
+            this.type = type;
+        }
+        public enum ContainerType
+        {
+            Regular,
+            Cooled,
+            Precious
+        }
+    }
+}
